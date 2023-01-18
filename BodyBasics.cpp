83672db40ba8bd5ba38dd90ad6d716079f92c2b6
Bohdan213 <93724511+Bohdan213@users.noreply.github.com>
2023-01-18 14:14:48 +0200

    Update BodyBasics.cpp

--- BodyBasics.cpp
+++ BodyBasics.cpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include "stdafx.h"
 #include "SimpleSerial.h"
+//#include <opencv2/opencv.hpp>
 
 #define PI 3.14159265359
 
@@ -14,21 +15,21 @@ DWORD COM_BAUD_RATE = CBR_9600;
 SimpleSerial Serial2(com_port2, COM_BAUD_RATE);
 std::string my_data2;
 
-char com_port1[] = "\\\\.\\COM7";
+char com_port1[] = "\\\\.\\COM8";
 SimpleSerial Serial1(com_port1, COM_BAUD_RATE);
 std::string my_data1;
 
 char com_port3[] = "\\\\.\\COM3";
 SimpleSerial Serial3(com_port3, COM_BAUD_RATE);
 
-int location1 = 0, location2 = 0;
-int scale1 = 0, scale2 = 0;
+int location1_x = 0, location1_y = 0, location2_x = 0, location2_y = 0;
+int scale1_x = 0, scale1_y = 0, scale2_x = 0, scale2_y = 0;
 
-double coefficients[25] = {0.003, 0.013, 0.022, 0.013, 0.003, 0.013, 0.06, 0.098, 0.06, 0.013, 0.022, 0.098, 0.162, 0.098, 0.022, 0.013, 0.06, 0.098, 0.06, 0.013, 0.003, 0.013, 0.022, 0.013, 0.003};
+double coefficients[25] = { 0.003, 0.013, 0.022, 0.013, 0.003, 0.013, 0.06, 0.098, 0.06, 0.013, 0.022, 0.098, 0.162, 0.098, 0.022, 0.013, 0.06, 0.098, 0.06, 0.013, 0.003, 0.013, 0.022, 0.013, 0.003 };
 
 IBodyFrameSource* pBodyFrameSource = nullptr;
 
-void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint, bool> > &targets) {
+void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint, bool> >& targets) {
     HRESULT hr = S_OK;
     IColorFrameSource* pColorFrameSource = nullptr;
     hr = pSensor->get_ColorFrameSource(&pColorFrameSource);
@@ -90,7 +91,7 @@ void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint,
                         hr = E_FAIL;
                     }
                 }
-                
+
                 for (auto& target : targets) {
                     ICoordinateMapper* pCoordinateMapper = nullptr;
                     pSensor->get_CoordinateMapper(&pCoordinateMapper);
@@ -101,8 +102,9 @@ void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint,
                     int x = (int)(colorSpacePoint.X + 0.5f);
                     int y = (int)(colorSpacePoint.Y + 0.5f);
 
+
                     if (x >= 0 && x < nWidth && y >= 0 && y < nHeight) {
-                        
+
                         int red = 0;
                         int green = 0;
                         int blue = 0;
@@ -116,11 +118,11 @@ void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint,
                                 counter++;
                             }
                         }
-                        
+
                         string data = "";
                         data += to_string(red) + ' ' + to_string(green) + ' ' + to_string(blue) + '\n';
                         OutputDebugStringA(data.c_str());
-                        if (red > 110 && blue < 70 && green < 70) {
+                        if (red > 100 && blue < 80 && green < 80) {
                             get<1>(target) = true;
                         }
                     }
@@ -132,11 +134,11 @@ void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint,
     delete[] m_pColorRGBX;
 }
 
-void toStream(std::vector<std::tuple<CameraSpacePoint, bool> > &targets) {
+void toStream(std::vector<std::tuple<CameraSpacePoint, bool> >& targets) {
 
     if (targets.size() != 0) {
 
-        // 1 port
+        // 3 port
 
         float x_data1 = get<0>(targets[0]).X;
         float y_data1 = get<0>(targets[0]).Y;
@@ -160,16 +162,18 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> > &targets) {
 
         if (state1) { my_data1 = "L1X"; }
         else { my_data1 = "X"; }
-        my_data1 += to_string((int)(120 - 120 * x_axis1 * scale1 / PI + location1));
+        float new_scale1_x = scale1_x / 100.0;
+        my_data1 += to_string((int)(120 - 120 * x_axis1 * new_scale1_x / PI) + location1_x);
         my_data1 += "Y";
-        my_data1 += to_string((int)(120 * y_axis1 * scale1 / PI + location1));
+        float new_scale1_y = scale1_y / 100.0;
+        my_data1 += to_string((int)(120 * y_axis1 * new_scale1_y / PI) + location1_y);
         //OutputDebugStringA(debug_str1.c_str());
         char* new_data1 = new char[my_data1.size()];
         for (int i = 0; i < my_data1.size(); ++i) {
             new_data1[i] = my_data1[i];
         }
 
-        // 2 port
+        // 4 port
 
         float x_data2 = get<0>(targets[targets.size() - 1]).X;
         float y_data2 = get<0>(targets[targets.size() - 1]).Y;
@@ -194,9 +198,11 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> > &targets) {
 
         if (state2) { my_data2 = "L1X"; }
         else { my_data2 = "X"; }
-        my_data2 += to_string((int)(120 - 120 * x_axis2 * scale2 / PI + location2));
+        float new_scale2_x = scale2_x / 100.0;
+        my_data2 += to_string((int)(120 - 120 * x_axis2 * new_scale2_x / PI) + location2_x);
         my_data2 += "Y";
-        my_data2 += to_string((int)(120 * y_axis2 * scale2 / PI + location2));
+        float new_scale2_y = scale2_y / 100.0;
+        my_data2 += to_string((int)(120 * y_axis2 * new_scale2_y / PI ) + location2_y);
         //OutputDebugStringA(debug_str2.c_str());
         char* new_data2 = new char[my_data2.size()];
         for (int i = 0; i < my_data2.size(); ++i) {
@@ -209,7 +215,6 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> > &targets) {
             OutputDebugStringA("Connected 7 PORT!!!");
             Serial1.WriteSerialPort(new_data1);
         }
-
         if (Serial2.connected_) {
             OutputDebugStringA("Connected 6 PORT!!!");
             Serial2.WriteSerialPort(new_data2);
@@ -218,52 +223,116 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> > &targets) {
     }
 }
 
+vector<int> readParams(int location_x, int location_y, int scale_x, int scale_y, string& parameters) {
 
+    size_t idx = 0;
+    int flag = 1;
+    while (parameters[idx] != ',') {
+        if (parameters[idx] == '-') {
+            flag = -1;
+        }
+        else {
+            location_x *= 10;
+            location_x += (parameters[idx] - '0');
+        }
+        idx++;
+    }
+    location_x *= flag;
+    flag = 1;
+    idx++;
+
+    while (parameters[idx] != ',') {
+        if (parameters[idx] == '-') {
+            flag = -1;
+        }
+        else {
+            location_y *= 10;
+            location_y += (parameters[idx] - '0');
+        }
+        idx++;
+    }
+    location_y *= flag;
+    flag = 1;
+    idx++;
+
+    while (parameters[idx] != ',') {
+        if (parameters[idx] == '-') {
+            flag = -1;
+        }
+        else {
+            scale_x *= 10;
+            scale_x += (parameters[idx] - '0');
+        }
+        idx++;
+    }
+    scale_x *= flag;
+    idx++;
+    flag = 1;
+
+    while (idx < parameters.size()) {
+        if (parameters[idx] == '-') {
+            flag = -1;
+        }
+        else {
+            scale_y *= 10;
+            scale_y += (parameters[idx] - '0');
+        }
+        idx++;
+    }
+    scale_y *= flag;
+    vector<int> v;
+    v.push_back(location_x);
+    v.push_back(location_y);
+    v.push_back(scale_x);
+    v.push_back(scale_y);
+    return v;
+}
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     while (!Serial1.connected_ && !Serial2.connected_ && !Serial3.connected_) {
         ;
     }
-    
+
     while (1) {
         int reply_wait_time = 1;
         string syntax_type = "greater_less_than";
-
+        string parametrs1 = "";
+        string parametrs2 = "";
         string incoming = Serial3.ReadSerialPort(reply_wait_time, syntax_type);
-        if (incoming == "S")
-            Serial1.WriteSerialPort("D");
-            string parametrs1 = Serial1.ReadSerialPort(reply_wait_time, syntax_type);
-            int idx = 0;
-            while (parametrs1[idx] != ',') {
-                location1 *= 10;
-                location1 += parametrs1[idx] - '0';
-                idx++;
+        char* d = new char[1];
+        d[0] = 'D';
+        if (incoming == "S") {
+            while (1) {
+                Serial1.WriteSerialPort(d);
+                parametrs1 = Serial1.ReadSerialPort(reply_wait_time, syntax_type);
+                if (parametrs1 != "") {
+                    break;
+                }
             }
-            idx++;
-            while (idx != parametrs1.size()) {
-                scale1 *= 10;
-                scale1 += parametrs1[idx] - '0';
-                idx++;
+            vector<int> v1 = readParams(location1_x, location1_y, scale1_x, scale1_y, parametrs1);
+            location1_x = v1[0];
+            location1_y = v1[1];
+            scale1_x = v1[2];
+            scale1_y = v1[3];
+            Sleep(20);
+            while (1) {
+                Serial2.WriteSerialPort(d);
+                parametrs2 = Serial2.ReadSerialPort(reply_wait_time, syntax_type);
+                if (parametrs2 != "") {
+                    break;
+                }
             }
-
-            Serial2.WriteSerialPort("D");
-            string parametrs2 = Serial2.ReadSerialPort(reply_wait_time, syntax_type);
-            int idx = 0;
-            while (parametrs2[idx] != ',') {
-                location2 *= 10;
-                location2 += parametrs2[idx] - '0';
-                idx++;
-            }
-            idx++;
-            while (idx != parametrs2.size()) {
-                scale2 *= 10;
-                scale2 += parametrs2[idx] - '0';
-                idx++;
-            }
-
+            vector<int> v2 = readParams(location2_x, location2_y, scale2_x, scale2_y, parametrs2);
+            location2_x = v2[0];
+            location2_y = v2[1];
+            scale2_x = v2[2];
+            scale2_y = v2[3];
+            Sleep(20);
             break;
+        }
     }
+
 
     IKinectSensor* pSensor = nullptr;
     HRESULT hr = S_OK;
@@ -284,6 +353,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     {
         hr = pBodyFrameSource->OpenReader(&pBodyFrameReader);
     }
+
 
     IBody* pBody[BODY_COUNT] = { 0 };
     while (true)
@@ -317,7 +387,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                             hr = Body->GetJoints(_countof(joints), joints);
                             if (SUCCEEDED(hr))
                             {
-                                    targets.push_back(std::make_tuple(joints[JointType_SpineMid].Position, false));
+                                targets.push_back(std::make_tuple(joints[JointType_SpineShoulder].Position, false));
                             }
                         }
                     }
