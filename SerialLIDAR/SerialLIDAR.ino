232924768a15497758930318029100d21ad6a505
maxmyk <91616527+maxmyk@users.noreply.github.com>
2023-01-18 14:31:03 +0200

    Update SerialLIDAR.ino

--- SerialLIDAR/SerialLIDAR.ino
+++ SerialLIDAR/SerialLIDAR.ino
@@ -1,107 +1,89 @@
-#include<Servo.h>
-#include <EEPROM.h>
+#include <stdint.h>
+#include <Wire.h>
+#include <LIDARLite_v3HP.h>
 
-Servo serX;
-Servo serY;
+LIDARLite_v3HP myLidarLite;
 
-String serialData;
+#define FAST_I2C
 
-void setup() {
-  pinMode(11, OUTPUT);
-  serX.attach(9);
-  serY.attach(10);
-  digitalWrite(11, 1);   // turn the LED on (HIGH is the voltage level)
-  serX.write(60);
-  serY.write(60);
-  delay(500);                       // wait for a second
-  digitalWrite(11, 0);    // turn the LED off by making the voltage LOW
+void gameover(){
+  int melody[] = {
+    262, 247, 233, 220
+  };
+  int noteDurations[] = {
+    2, 2, 2, 1
+  };
+  for (int thisNote = 0; thisNote < 4; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+}
+
+void set_siren(int runtime){
+  runtime /= 200;
+  for(int i =0;i<runtime;++i){
+    for(int hz = 2000; hz < 2500; hz+=25){
+      tone(8, hz, 50);
+      delay(5);
+    }
+    for(int hz = 2500; hz > 2000; hz-=25){
+      tone(8, hz, 50);
+      delay(5);
+    }
+  }
+}
+
+void setup()
+{
   Serial.begin(9600);
-  Serial.setTimeout(10);
+  Wire.begin();
+  #ifdef FAST_I2C
+    #if ARDUINO >= 157
+      Wire.setClock(400000UL);
+    #else
+      TWBR = ((F_CPU / 400000UL) - 16) / 2;
+    #endif
+  #endif
+  myLidarLite.configure(0);
 }
 
-void loop() {
-  //lol
-}
 
-void serialEvent() {
-  serialData = Serial.readString();
-  int values[4];
-  if (parseDataM(serialData, values)){
-    writeValuesToEEPROM(values);
-  }
-  if(parseDataD(serialData)){
-    digitalWrite(11, parseDataL(serialData));
-    serX.write(parseDataX(serialData));
-    serY.write(parseDataY(serialData));
-  }
-}
-
-int parseDataL(String data) {
-  data.remove(data.indexOf("X"));
-  data.remove(data.indexOf("L"), 1);
-  return data.toInt();
-}
-
-int parseDataX(String data) {
-  data.remove(data.indexOf("Y"));
-  data.remove(data.indexOf("X"), 1);
-  data.remove(data.indexOf("L"), 2);
-  return data.toInt();
-}
-
-int parseDataY(String data) {
-  data.remove(0, data.indexOf("Y") + 1);
-  return data.toInt();
-}
-
-uint8_t parseDataM(String data, int * values){
-  uint8_t flag = 0;
-  if (data[0] == 'M') {
-    int index = 0;
-    String currentValue = "";
-    bool negative = false;
-    flag = 1;
-    for (int i = 2; i < data.length(); i++) {
-      if(data[i] == '-'){
-        negative = true;
-        continue;
-      }
-      if (data[i] == ',') {
-        values[index++] = (negative) ? -currentValue.toInt(): currentValue.toInt();
-        currentValue = "";
-        negative = false;
-      } else {
-        currentValue += data[i];
+void loop()
+{
+  uint8_t distance;
+  uint8_t newDistance = 0;
+  uint8_t oldDistance;
+  myLidarLite.waitForBusy();
+  myLidarLite.takeRange();
+  myLidarLite.waitForBusy();
+  oldDistance = myLidarLite.readDistance();
+  Serial.println(oldDistance);
+  Serial.println("Start:");
+  uint8_t acceptableError = 10;
+  while (1)
+  {
+    newDistance = distanceContinuous(&distance);
+    if (newDistance && distance<1000)
+    {
+      if (abs(distance - oldDistance) > acceptableError) {
+        Serial.println("SIREN");
+        set_siren(1000);
       }
     }
-    values[index] = (negative) ? -currentValue.toInt(): currentValue.toInt();
+    delay(10);
   }
-  return flag;
 }
-
-int parseDataD(String data) {
-  int ans = 1;
-  if (data[0] == 'D') {
-    ans = 0;
-    Serial.print('<');
-    for (int i = 0; i < 4; ++i) {
-      byte sign = EEPROM.read(i * 2);
-      byte value = EEPROM.read(i * 2 + 1);
-      Serial.print((sign == 1) ? -value : value);
-      if(i<3)
-        Serial.print(',');
-    }
-    Serial.println('>');
+uint8_t distanceContinuous(uint8_t * distance)
+{
+  uint8_t newDistance = 0;
+  if (myLidarLite.getBusyFlag() == 0)
+  {
+    myLidarLite.takeRange();
+    *distance = myLidarLite.readDistance();
+    newDistance = 1;
   }
-  return ans;
-}
-
-void writeValuesToEEPROM(int* values) {
-  for (int i = 0; i < 4; i++) {
-    int value = values[i];
-    byte sign = (value < 0) ? 1 : 0;
-    value = abs(value);
-    EEPROM.write(i * 2, sign);
-    EEPROM.write(i * 2 + 1, (byte)value);
-  }
+  return newDistance;
 }
