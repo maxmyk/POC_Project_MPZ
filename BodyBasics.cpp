8d0b8318c4833af091b5bf27afdf1751723992dd
maxmyk <maksym.mykhasiuta@ucu.edu.ua>
2023-04-01 13:41:09 +0300

    Post-final

--- BodyBasics.cpp
+++ BodyBasics.cpp
@@ -6,24 +6,22 @@
 #include <tuple>
 #include "stdafx.h"
 #include "SimpleSerial.h"
-//#include <opencv2/opencv.hpp>
 
 #define PI 3.14159265359
 
-char com_port2[] = "\\\\.\\COM6";
+char com_port2[] = "\\\\.\\COM14";
 DWORD COM_BAUD_RATE = CBR_9600;
 SimpleSerial Serial2(com_port2, COM_BAUD_RATE);
 std::string my_data2;
 
-char com_port1[] = "\\\\.\\COM8";
+char com_port1[] = "\\\\.\\COM5";
 SimpleSerial Serial1(com_port1, COM_BAUD_RATE);
 std::string my_data1;
 
-char com_port3[] = "\\\\.\\COM3";
+char com_port3[] = "\\\\.\\COM13";
 SimpleSerial Serial3(com_port3, COM_BAUD_RATE);
 
-int location1_x = 0, location1_y = 0, location2_x = 0, location2_y = 0;
-int scale1_x = 0, scale1_y = 0, scale2_x = 0, scale2_y = 0;
+double location1_x = 0, location1_y = 0, location2_x = 0, location2_y = 0, location1_z = 0, location2_z = 0;
 
 double coefficients[25] = { 0.003, 0.013, 0.022, 0.013, 0.003, 0.013, 0.06, 0.098, 0.06, 0.013, 0.022, 0.098, 0.162, 0.098, 0.022, 0.013, 0.06, 0.098, 0.06, 0.013, 0.003, 0.013, 0.022, 0.013, 0.003 };
 
@@ -121,6 +119,7 @@ void colorFrame(IKinectSensor* pSensor, std::vector<std::tuple<CameraSpacePoint,
 
                         string data = "";
                         data += to_string(red) + ' ' + to_string(green) + ' ' + to_string(blue) + '\n';
+                        OutputDebugStringA("Colors");
                         OutputDebugStringA(data.c_str());
                         if (red > 100 && blue < 80 && green < 80) {
                             get<1>(target) = true;
@@ -138,7 +137,9 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> >& targets) {
 
     if (targets.size() != 0) {
 
-        // 3 port
+        /*
+        First turret
+        */
 
         float x_data1 = get<0>(targets[0]).X;
         float y_data1 = get<0>(targets[0]).Y;
@@ -146,34 +147,40 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> >& targets) {
         bool state1 = get<1>(targets[0]);
 
         std::string debug_str1;
-        float x_axis1 = 0.0;
-        float y_axis1 = 0.0;
-        float z_axis1 = 0.0;
+        float x_axis1 = 90.0;
+        float y_axis1 = 90.0;
+
         if ((x_data1 * x_data1 + y_data1 * y_data1 + z_data1 * z_data1) != 0) {
-            x_axis1 = acos(x_data1 / sqrtf(x_data1 * x_data1 + y_data1 * y_data1 + z_data1 * z_data1));
-            y_axis1 = acos(y_data1 / sqrtf(x_data1 * x_data1 + y_data1 * y_data1 + z_data1 * z_data1));
+            x_axis1 = (int)(180 + 180 * atan(abs(location1_z - z_data1) / (location1_x - x_data1)) / PI) % 180;
+            y_axis1 = (int)(90 - 180 * atan((location1_y - y_data1) / sqrt(pow(location1_z - z_data1, 2) + pow(location1_x - x_data1, 2))) / PI)% 180;
         }
-        debug_str1 += to_string(120 * x_axis1 / PI);
+
+        debug_str1 += to_string(z_data1); // y
         debug_str1 += " ";
-        debug_str1 += to_string((120 - 120 * y_axis1) / PI);
+        debug_str1 += to_string(x_data1); // z
         debug_str1 += " ";
-        debug_str1 += to_string(70 + 180 * z_axis1 / PI);
+        debug_str1 += to_string(y_data1); // x
+        debug_str1 += "\n :^)";
+        debug_str1 += to_string(x_axis1);
+        debug_str1 += " ";
+        debug_str1 += to_string(y_axis1);
         debug_str1 += "\n";
+        OutputDebugStringA(debug_str1.c_str());
 
         if (state1) { my_data1 = "L1X"; }
         else { my_data1 = "X"; }
-        float new_scale1_x = scale1_x / 100.0;
-        my_data1 += to_string((int)(120 - 120 * x_axis1 * new_scale1_x / PI) + location1_x);
+        my_data1 += to_string((int)x_axis1);
         my_data1 += "Y";
-        float new_scale1_y = scale1_y / 100.0;
-        my_data1 += to_string((int)(120 * y_axis1 * new_scale1_y / PI) + location1_y);
-        //OutputDebugStringA(debug_str1.c_str());
-        char* new_data1 = new char[my_data1.size()];
-        for (int i = 0; i < my_data1.size(); ++i) {
-            new_data1[i] = my_data1[i];
-        }
+        my_data1 += to_string((int)y_axis1);
 
-        // 4 port
+        OutputDebugStringA(my_data1.c_str());
+
+        char* new_data1 = new char[my_data1.length() + 1];
+        strcpy(new_data1, my_data1.c_str());
+
+        /*
+        Second turret
+        */
 
         float x_data2 = get<0>(targets[targets.size() - 1]).X;
         float y_data2 = get<0>(targets[targets.size() - 1]).Y;
@@ -181,111 +188,75 @@ void toStream(std::vector<std::tuple<CameraSpacePoint, bool> >& targets) {
         bool state2 = get<1>(targets[targets.size() - 1]);
 
         std::string debug_str2;
-        float x_axis2 = 0.0;
-        float y_axis2 = 0.0;
-        float z_axis2 = 0.0;
-        if ((x_data2 * x_data2 + y_data2 * y_data2 + z_data2 * z_data2) != 0) {
-            x_axis2 = acos(x_data2 / sqrtf(x_data2 * x_data2 + y_data2 * y_data2 + z_data2 * z_data2));
-            y_axis2 = acos(y_data2 / sqrtf(x_data2 * x_data2 + y_data2 * y_data2 + z_data2 * z_data2));
-        }
-        debug_str2 += to_string(120 * x_axis2 / PI);
-        debug_str2 += " ";
-        debug_str2 += to_string((120 - 120 * y_axis2) / PI);
-        debug_str2 += " ";
-        debug_str2 += to_string(70 + 180 * z_axis2 / PI);
-        debug_str2 += "\n";
+        float x_axis2 = 90.0;
+        float y_axis2 = 90.0;
 
+        if ((x_data2 * x_data2 + y_data2 * y_data2 + z_data2 * z_data2) != 0) {
+            x_axis2 = (int)(180 + 180 * atan(abs(location2_z - z_data2) / (location2_x - x_data2)) / PI) % 180;
+            y_axis2 = (int)(90 + 180 * atan((location2_y - y_data2) / sqrt(pow(location2_z - z_data2, 2) + pow(location2_x - x_data2, 2))) / PI) % 180;
+        }
+
+        debug_str2 += to_string(z_data2); // y
+        debug_str2 += " ";
+        debug_str2 += to_string(x_data2); // z
+        debug_str2 += " ";
+        debug_str2 += to_string(y_data2); // x
+        debug_str2 += "\n :^)";
+        debug_str2 += to_string(x_axis2);
+        debug_str2 += " ";
+        debug_str2 += to_string(y_axis2);
+        debug_str2 += "\n";
+        OutputDebugStringA(debug_str2.c_str());
 
         if (state2) { my_data2 = "L1X"; }
         else { my_data2 = "X"; }
-        float new_scale2_x = scale2_x / 100.0;
-        my_data2 += to_string((int)(120 - 120 * x_axis2 * new_scale2_x / PI) + location2_x);
+        my_data2 += to_string((int)x_axis2);
         my_data2 += "Y";
-        float new_scale2_y = scale2_y / 100.0;
-        my_data2 += to_string((int)(120 * y_axis2 * new_scale2_y / PI ) + location2_y);
-        //OutputDebugStringA(debug_str2.c_str());
-        char* new_data2 = new char[my_data2.size()];
-        for (int i = 0; i < my_data2.size(); ++i) {
-            new_data2[i] = my_data2[i];
-        }
+        my_data2 += to_string((int)y_axis2);
+
+        OutputDebugStringA(my_data2.c_str());
+
+        char* new_data2 = new char[my_data2.length() + 1];
+        strcpy(new_data2, my_data2.c_str());
 
         // data send
 
         if (Serial1.connected_) {
-            OutputDebugStringA("Connected 7 PORT!!!");
+            OutputDebugStringA("\nTurret 1 Connected\n");
             Serial1.WriteSerialPort(new_data1);
+            OutputDebugStringA(new_data1);
         }
         if (Serial2.connected_) {
-            OutputDebugStringA("Connected 6 PORT!!!");
+            OutputDebugStringA("\nTurret 2 Connected\n");
             Serial2.WriteSerialPort(new_data2);
+            OutputDebugStringA(new_data2);
+            OutputDebugStringA(" \n ");
         }
         Sleep(15);
     }
 }
 
-vector<int> readParams(int location_x, int location_y, int scale_x, int scale_y, string& parameters) {
-
-    size_t idx = 0;
-    int flag = 1;
-    while (parameters[idx] != ',') {
-        if (parameters[idx] == '-') {
-            flag = -1;
+vector<double> readParams(string& parameters) {
+//    Reads parameters from string and returns vector of doubles
+//    Parameters are given as ints in such format: <25,-35,45,0>
+//    Parameters are: x, y, z, free
+    vector<double> params;
+    string param = "";
+    for (int i = 0; i < parameters.length(); i++) {
+        if (parameters[i] == ',') {
+            params.push_back(stod(param)/100);
+            param = "";
+        }
+        else if (parameters[i] == '>') {
+            params.push_back(stod(param)/100);
+            param = "";
+            break;
         }
         else {
-            location_x *= 10;
-            location_x += (parameters[idx] - '0');
+            param += parameters[i];
         }
-        idx++;
     }
-    location_x *= flag;
-    flag = 1;
-    idx++;
-
-    while (parameters[idx] != ',') {
-        if (parameters[idx] == '-') {
-            flag = -1;
-        }
-        else {
-            location_y *= 10;
-            location_y += (parameters[idx] - '0');
-        }
-        idx++;
-    }
-    location_y *= flag;
-    flag = 1;
-    idx++;
-
-    while (parameters[idx] != ',') {
-        if (parameters[idx] == '-') {
-            flag = -1;
-        }
-        else {
-            scale_x *= 10;
-            scale_x += (parameters[idx] - '0');
-        }
-        idx++;
-    }
-    scale_x *= flag;
-    idx++;
-    flag = 1;
-
-    while (idx < parameters.size()) {
-        if (parameters[idx] == '-') {
-            flag = -1;
-        }
-        else {
-            scale_y *= 10;
-            scale_y += (parameters[idx] - '0');
-        }
-        idx++;
-    }
-    scale_y *= flag;
-    vector<int> v;
-    v.push_back(location_x);
-    v.push_back(location_y);
-    v.push_back(scale_x);
-    v.push_back(scale_y);
-    return v;
+    return params;
 }
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
@@ -299,10 +270,20 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         string syntax_type = "greater_less_than";
         string parametrs1 = "";
         string parametrs2 = "";
-        string incoming = Serial3.ReadSerialPort(reply_wait_time, syntax_type);
+        string incoming = "";
+        while (1) {
+            incoming = Serial3.ReadSerialPort(reply_wait_time, syntax_type);
+            if (incoming != "") {
+                OutputDebugStringA(incoming.c_str());
+                break;
+            }
+            else {
+                OutputDebugStringA(".");
+            }
+        }
         char* d = new char[1];
         d[0] = 'D';
-        if (incoming == "S") {
+        if (incoming == "SIREN") {
             while (1) {
                 Serial1.WriteSerialPort(d);
                 parametrs1 = Serial1.ReadSerialPort(reply_wait_time, syntax_type);
@@ -310,11 +291,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                     break;
                 }
             }
-            vector<int> v1 = readParams(location1_x, location1_y, scale1_x, scale1_y, parametrs1);
+            OutputDebugStringA("A");
+            vector<double> v1 = readParams(parametrs1);
             location1_x = v1[0];
             location1_y = v1[1];
-            scale1_x = v1[2];
-            scale1_y = v1[3];
+            location1_z = v1[2];
             Sleep(20);
             while (1) {
                 Serial2.WriteSerialPort(d);
@@ -323,12 +304,13 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                     break;
                 }
             }
-            vector<int> v2 = readParams(location2_x, location2_y, scale2_x, scale2_y, parametrs2);
+            OutputDebugStringA("B");
+            vector<double> v2 = readParams(parametrs2);
             location2_x = v2[0];
             location2_y = v2[1];
-            scale2_x = v2[2];
-            scale2_y = v2[3];
+            location2_z = v2[2];
             Sleep(20);
+            OutputDebugStringA("C");
             break;
         }
     }
--- SerialReader/SerialReader.ino
+++ SerialReader/SerialReader.ino
@@ -7,101 +7,104 @@ Servo serY;
 String serialData;
 
 void setup() {
-  pinMode(11, OUTPUT);
-  serX.attach(9);
-  serY.attach(10);
-  digitalWrite(11, 1);   // turn the LED on (HIGH is the voltage level)
-  serX.write(60);
-  serY.write(60);
-  delay(500);                       // wait for a second
-  digitalWrite(11, 0);    // turn the LED off by making the voltage LOW
-  Serial.begin(9600);
-  Serial.setTimeout(10);
+    pinMode(11, OUTPUT);
+    serX.attach(9);
+    serY.attach(10);
+    digitalWrite(11, 1);   // turn the LED on (HIGH is the voltage level)
+    serX.write(90);
+    serY.write(90);
+    delay(500);                       // wait for a second
+    digitalWrite(11, 0);    // turn the LED off by making the voltage LOW
+    Serial.begin(9600);
+    Serial.setTimeout(10);
 }
 
 void loop() {
-  //lol
+    //lol
 }
 
 void serialEvent() {
-  serialData = Serial.readString();
-  int values[4];
-  if (parseDataM(serialData, values)){
-    writeValuesToEEPROM(values);
-  }
-  if(parseDataD(serialData)){
-    digitalWrite(11, parseDataL(serialData));
-    serX.write(parseDataX(serialData));
-    serY.write(parseDataY(serialData));
-  }
+    serialData = Serial.readString();
+    int values[4];
+    if (parseDataM(serialData, values)) {
+        for (int i = 0; i < 4; ++i) {
+            Serial.print(values[i]);
+            Serial.print(',');
+        }
+        writeValuesToEEPROM(values);
+    }
+    if (!parseDataD(serialData)) {
+        digitalWrite(11, parseDataL(serialData));
+        serX.write(parseDataX(serialData));
+        serY.write(parseDataY(serialData));
+    }
 }
 
 int parseDataL(String data) {
-  data.remove(data.indexOf("X"));
-  data.remove(data.indexOf("L"), 1);
-  return data.toInt();
+    data.remove(data.indexOf("X"));
+    data.remove(data.indexOf("L"), 1);
+    return data.toInt();
 }
 
 int parseDataX(String data) {
-  data.remove(data.indexOf("Y"));
-  data.remove(data.indexOf("X"), 1);
-  data.remove(data.indexOf("L"), 2);
-  return data.toInt();
+    data.remove(data.indexOf("Y"));
+    data.remove(data.indexOf("X"), 1);
+    data.remove(data.indexOf("L"), 2);
+    return data.toInt();
 }
 
 int parseDataY(String data) {
-  data.remove(0, data.indexOf("Y") + 1);
-  return data.toInt();
+    data.remove(0, data.indexOf("Y") + 1);
+    return data.toInt();
 }
 
-uint8_t parseDataM(String data, int * values){
-  uint8_t flag = 0;
-  if (data[0] == 'M') {
-    int index = 0;
-    String currentValue = "";
-    bool negative = false;
-    flag = 1;
-    for (int i = 2; i < data.length(); i++) {
-      if(data[i] == '-'){
-        negative = true;
-        continue;
-      }
-      if (data[i] == ',') {
-        values[index++] = (negative) ? -currentValue.toInt(): currentValue.toInt();
-        currentValue = "";
-        negative = false;
-      } else {
-        currentValue += data[i];
-      }
+uint8_t parseDataM(String data, int *values) {
+    uint8_t flag = 0;
+    if (data[0] == 'M') {
+        flag = 1;
+        data.remove(0, 1);
+        for (int i = 0; i < 4; ++i) {
+            values[i] = data.substring(0, data.indexOf(',')).toInt();
+            data.remove(0, data.indexOf(',') + 1);
+        }
     }
-    values[index] = (negative) ? -currentValue.toInt(): currentValue.toInt();
-  }
-  return flag;
+    return flag;
 }
 
-int parseDataD(String data) {
-  int ans = 1;
-  if (data[0] == 'D') {
-    ans = 0;
-    Serial.print('<');
-    for (int i = 0; i < 4; ++i) {
-      byte sign = EEPROM.read(i * 2);
-      byte value = EEPROM.read(i * 2 + 1);
-      Serial.print((sign == 1) ? -value : value);
-      if(i<3)
-        Serial.print(',');
+uint8_t parseDataD(String data) {
+    uint8_t flag = 0;
+    int my_values[4];
+    if (data[0] == 'D') {
+        flag = 1;
+        EEPROM_read(EEPROM_read(EEPROM_read(EEPROM_read(0, my_values[0]), my_values[1]), my_values[2]), my_values[3]);
+        Serial.print('<');
+        for (int i = 0; i < 4; ++i) {
+            Serial.print(my_values[i]);
+            if (i < 3) Serial.print(',');
+        }
+        Serial.println('>');
     }
-    Serial.println('>');
-  }
-  return ans;
+    return flag;
 }
 
-void writeValuesToEEPROM(int* values) {
-  for (int i = 0; i < 4; i++) {
-    int value = values[i];
-    byte sign = (value < 0) ? 1 : 0;
-    value = abs(value);
-    EEPROM.write(i * 2, sign);
-    EEPROM.write(i * 2 + 1, (byte)value);
-  }
+void writeValuesToEEPROM(int *values) {
+    EEPROM_write(EEPROM_write(EEPROM_write(EEPROM_write(0, values[0]), values[1]), values[2]), values[3]);
+}
+
+template<class T>
+int EEPROM_write(int ee, const T &value) {
+    const byte *p = (const byte *) (const void *) &value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+        EEPROM.write(ee++, *p++);
+    return ee;
+}
+
+template<class T>
+int EEPROM_read(int ee, T &value) {
+    byte *p = (byte * )(void * ) & value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+        *p++ = EEPROM.read(ee++);
+    return ee;
 }
